ca9f767ee2e4f137bc4434e33e61c1ae63aafa47
cikent <christopherikent@gmail.com>
2020-03-01 21:47:13 -0800

    New Lesson 14, completion of myPets.py
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_13_Lists_Indexes_Slices/allMyCats2.py
+++ Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_13_Lists_Indexes_Slices/allMyCats2.py
@@ -1,17 +1,23 @@
 # Declare an empty List called catNames
 catNames = []
 
+
 while True:
+    # Ask the User for the Catz name?
     print(
         "Enter the name of cat "
         + str(len(catNames) + 1)
         + " (Or enter nothing to stop.):"
     )
+    # Store the User's response 
     name = input()
+    # Evaluate User's response; If blank, break and exit the While loop
     if name == "":
         break
-    catNames = catNames + [name]  # list con'cat'entation :)
+    # Update catNames by appending the new name to the end of the list
+    catNames = catNames + [name]  
 
+# Print the Cat names after the break in the While loop 
 print("The cat names are:")
 for name in catNames:
     print("    " + name)
