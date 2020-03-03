1fb1a0a888630bb84c75991e22568097ab00b186
cikent <christopherikent@gmail.com>
2020-03-02 23:10:50 -0800

    Added Comments to Magic8Ball2.py & continued studying
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/magic8Ball2.py
+++ Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/magic8Ball2.py
@@ -1,16 +1,17 @@
+# import the Library: random
 import random
 
-messages = [
-    "It is certain",
-    "It is decidedly so",
-    "Yes definitely",
-    "Reply hazy try again",
-    "Ask again later",
-    "Concentrate and ask again",
-    "My reply is no",
-    "Outlook not so good",
-    "Very doubtful",
-]
+# Declare a list of messages
+messages = ['It is certain',
+    'It is decidedly so',
+    'Yes definitely',
+    'Reply hazy try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not so good',
+    'Very doubtful']
 
+# Print a random value from the list ranging between 0 and 8 (9 Total)
 print(messages[random.randint(0, len(messages) - 1)])
 
