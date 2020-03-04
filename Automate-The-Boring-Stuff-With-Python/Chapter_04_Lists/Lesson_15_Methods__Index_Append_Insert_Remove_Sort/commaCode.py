698854fb257bd8e185ee0e15146762af58ddfe56
cikent <christopherikent@gmail.com>
2020-03-03 23:06:09 -0800

    Update to commaCode.py; still need to inject 'and' before last index
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/commaCode.py
+++ Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/commaCode.py
@@ -1,1 +1,28 @@
 # Start of Practice Projects for Chapter 4
+""" Requirements """
+# Creat a function that takes a List as an Argument
+# Return a String with all the Items in the List concatenated by: ', ' (Comma & a Space)
+# Insert 'and' before the last Item is concatenated with the String
+# Function should be able to work with any List value pass to it
+
+# sample test list
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+
+
+def mergeList(anyList):
+    # Declare an empty String    
+    mergeWords = ''
+    # For each word in the List, add it to mergeWords
+    for item in anyList:
+        # todo
+        mergeWords += str(item + ', ')
+        # mergeWords = mergeWords + str(anyList[len(anyList)])      # Comment out for debugging
+    
+    return mergeWords
+
+print(spam)
+
+newMessage = mergeList(spam)
+
+print(newMessage)
