cb3757ac540a146c1052e44cd635be84d275df8e
Christopher Kent <christopherikent@gmail.com>
2020-03-04 13:00:31 -0800

    Finished commaCode.py and started on characterPictureGrid.py
    Signed-off-by: Christopher Kent <christopherikent@gmail.com>

---
+++ Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/characterPictureGrid.py
@@ -0,0 +1,22 @@
+"""
+Requirements: 
+1. Creat a function that takes a List as an Argument
+2. Return a String with all the Items in the List concatenated by: ', ' (Comma & a Space)
+3. Insert 'and' before the last Item is concatenated with the String
+4. Function should be able to work with any List value pass to it
+
+Source: https://automatetheboringstuff.com/chapter4/ -> search for Character Picture Grid
+"""
+
+# Define a List
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+print(grid)
--- Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/commaCode.py
+++ Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/commaCode.py
@@ -1,30 +1,42 @@
-# Start of Practice Projects for Chapter 4
-""" Requirements """
-# Creat a function that takes a List as an Argument
-# Return a String with all the Items in the List concatenated by: ', ' (Comma & a Space)
-# Insert 'and' before the last Item is concatenated with the String
-# Function should be able to work with any List value pass to it
+"""
+Requirements: 
+1. Creat a function that takes a List as an Argument
+2. Return a String with all the Items in the List concatenated by: ', ' (Comma & a Space)
+3. Insert 'and' before the last Item is concatenated with the String
+4. Function should be able to work with any List value pass to it
 
-# sample test list
-spam = ['apples', 'bananas', 'tofu', 'cats']
+Source: https://automatetheboringstuff.com/chapter4/ -> search for Comma Code
+"""
+
+# Sample test lists
+spamText = ['apples', 'bananas', 'tofu', 'cats']
+baconNumber = [1, 2, 3, 4, 5]
 
 
-
+# Define a function to take a List as a Parameter and concatenate the values of the List Reference together
 def mergeList(anyList):
     # Declare an empty String    
-    mergeWords = ''
-    # For each word in the List, add it to mergeWords
+    mergeWords = str('')
+    # For each word in the List, add it to mergeWords unless its the last index
     for item in anyList:
-        # todo
-        # if item != anyList[-1]: continue else: mergeWords += str(', and ' + item)
-        # 
-        mergeWords += str(item + ', ')
-        # mergeWords = mergeWords + str(anyList[len(anyList)])      # Comment out for debugging
-    
+        # If the item is not the last Index, then add the item followed by a Comma and a space 
+        if item != anyList[-1]:
+            mergeWords += str(item) + ', '
+        # If the item is the last Index, then add 'and' followed by the last item in the list
+        else: 
+            mergeWords += 'and ' + str(item)
+    # Return the new value of mergeWords
     return mergeWords
 
-print(spam)
+# Debug print statement to verify contents of variables are stored correctly
+print(spamText)
+print(baconNumber)
 
-newMessage = mergeList(spam)
+# Create new variables to hold the String value that mergeList will output
+newMessage = mergeList(spamText)
+newMessage2 = mergeList(baconNumber)
 
+
+# Print the value stored in the String variables
 print(newMessage)
+print(newMessage2)
