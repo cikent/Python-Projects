548f7ba0bb3bf428d954f2189422f0360e1c0040
cikent <christopherikent@gmail.com>
2020-03-04 22:33:03 -0800

    Finishing Chapter 4 for now; making Chapter 4's Character Picture Grid script a TODO so I can proceed onto other topics
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/characterPictureGrid.py
+++ Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/characterPictureGrid.py
@@ -1,9 +1,6 @@
 """
 Requirements: 
-1. Creat a function that takes a List as an Argument
-2. Return a String with all the Items in the List concatenated by: ', ' (Comma & a Space)
-3. Insert 'and' before the last Item is concatenated with the String
-4. Function should be able to work with any List value pass to it
+1. 
 
 Source: https://automatetheboringstuff.com/chapter4/ -> search for Character Picture Grid
 """
@@ -19,4 +16,4 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
-print(grid)
+print(str(grid))
--- Automate-The-Boring-Stuff-With-Python/README.md
+++ Automate-The-Boring-Stuff-With-Python/README.md
@@ -1,2 +1,5 @@
 # Automate-The-Boring-Stuff-With-Python
 Course work from Automate The Boring Stuff With Python by Al Sweigart
+
+# TODO:
+* Source: https://automatetheboringstuff.com/chapter4/ -> search for Character Picture Grid
