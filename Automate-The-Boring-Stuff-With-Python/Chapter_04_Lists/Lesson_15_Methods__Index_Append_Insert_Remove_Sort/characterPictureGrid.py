5a2bfbbf7f9504f18eee9def3cfb3644e007fa57
Christopher Kent <christopherikent@gmail.com>
2020-03-06 12:55:57 -0800

    Started on Character Picture Grid
    Signed-off-by: Christopher Kent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/characterPictureGrid.py
+++ Automate-The-Boring-Stuff-With-Python/Chapter_04_Lists/Lesson_15_Methods__Index_Append_Insert_Remove_Sort/characterPictureGrid.py
@@ -1,6 +1,13 @@
 """
 Requirements: 
-1. 
+1. Print Grid such that it outputs like:
+..OO.OO..
+.OOOOOOO.
+.OOOOOOO.
+..OOOOO..
+...OOO...
+....O....
+2. asdf
 
 Source: https://automatetheboringstuff.com/chapter4/ -> search for Character Picture Grid
 """
@@ -16,4 +23,14 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
+# Debug initial print of grid
 print(str(grid))
+
+# Define String variables to hold the grid
+gridRow = len(grid)
+gridColumn = len(grid[0])
+
+# Define a function to print the Grid
+def printGrid(gridList):
+        # Iterate through List(s)
+        for x in grid[x][y]:
