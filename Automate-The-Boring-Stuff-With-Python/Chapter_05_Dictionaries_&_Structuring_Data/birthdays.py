210b8321724eefb94e2e83a58036a0c6c9e45ce3
cikent <christopherikent@gmail.com>
2020-03-11 23:07:44 -0700

    Start of Chapter 5 -- Dictionaries and Structuring Data
    Signed-off-by: cikent <christopherikent@gmail.com>

---
+++ Automate-The-Boring-Stuff-With-Python/Chapter_05_Dictionaries_&_Structuring_Data/birthdays.py
@@ -0,0 +1,35 @@
+# Define a Dictionary to save the Birthday's of family
+birthdays = {'Nicole': 'May 20', 'Kaleb': 'Jan 10', 'Chris': 'Jul 15'}
+
+# Execute continuously until User enters nothing and the script exits
+while True:
+    # Ask the User to input a Name
+    print('Enter a name: (blank to quit)')
+    # Try clause to ensure the User inputs a valid entry
+    try:
+        # Save the User's input
+        name = str(input())
+    except:
+        # Prompt the User to retry if they input an invalid value
+        print('You didn\'t input a person\'s name, please try again.')
+    # Evaluate if the User entered anything
+    if name == '':
+        # If User entered nothing, exit script entirely
+        break
+    # Determine if the User's input matches a name preexisting within the Dictionary
+    if name in birthdays:
+        # If the name exists, print the Birthday(value) paired with the name(Key)
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+        # Otherwise, inform the User that name is not Stored
+        print('I do not have birthday information for ' + name)
+        print('What is their birthday?')
+        # Verify the User's input for the Birthday value is a String
+        try:
+            bday = str(input())
+        except:
+            print('That is not a valid Birthday. Please input their Birthday in the following format: 3 Letter Month & Day. Example: Jul 15')
+        # If User's input is a valid String, save it to the Birthday's Dictionary
+        birthdays[name] = bday
+        # Notify the User the Dictionary has been updated
+        print('Birthday database updated.')
--- Automate-The-Boring-Stuff-With-Python/README.md
+++ Automate-The-Boring-Stuff-With-Python/README.md
@@ -2,4 +2,5 @@
 Course work from Automate The Boring Stuff With Python by Al Sweigart
 
 ## TODO:
-* Source: https://automatetheboringstuff.com/chapter4/ -> search for Character Picture Grid
+* Contents in Chapter 5
+** Source: https://automatetheboringstuff.com/chapter5/
