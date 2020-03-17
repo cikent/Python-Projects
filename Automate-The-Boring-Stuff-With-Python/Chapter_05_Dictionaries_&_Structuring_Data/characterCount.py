b93044025f5fa873120c3c6cd05f6a5287832355
cikent <christopherikent@gmail.com>
2020-03-16 22:54:37 -0700

    Completion of Character Count && Pretty Character Count
    Signed-off-by: cikent <christopherikent@gmail.com>

---
+++ Automate-The-Boring-Stuff-With-Python/Chapter_05_Dictionaries_&_Structuring_Data/characterCount.py
@@ -0,0 +1,14 @@
+# Define a String
+message = 'It was a bright cold day in April, and the clocks were stricking thirteen.'
+# Define a empty Dictionary
+count = {}
+
+# Iterate through each character within the String 
+for character in message:
+    # Create a Key/value pair in the Count Dict for each unique Character value in message
+    count.setdefault(character, 0)
+    # Set the value of each Unique Key equal to the # of times that character is utilized within message
+    count[character] = count[character] + 1
+
+# Print to screen the value of Count
+print(count)
---
+++ Automate-The-Boring-Stuff-With-Python/Chapter_05_Dictionaries_&_Structuring_Data/prettyCharacterCount.py
@@ -0,0 +1,20 @@
+# Import Pprint and Pformat modules
+import pprint
+
+# Define a String
+message = 'It was a bright cold day in April, and the clocks were stricking thirteen.'
+# Define a empty Dictionary
+count = {}
+
+# Iterate through each character within the String 
+for character in message:
+    # Create a Key/value pair in the Count Dict for each unique Character value in message
+    count.setdefault(character, 0)
+    # Set the value of each Unique Key equal to the # of times that character is utilized within message
+    count[character] = count[character] + 1
+
+# Using the module Pprint's Pprint function, print to screen the value of Count in a nicely formatted style 
+pprint.pprint(count)
+
+# Using the module Pprint's pformat function, replicate the previous Pprint output using Pformat instead
+print(pprint.pformat(count))
