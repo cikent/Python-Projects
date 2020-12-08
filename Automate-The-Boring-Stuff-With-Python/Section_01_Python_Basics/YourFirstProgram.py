7e6cc4e0166bcb075c1ae518d435e4781af5ce12
Christopher Kent <christopherikent@gmail.com>
2020-12-08 07:57:35 -0800

    Udemy Course update
    Signed-off-by: Christopher Kent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Section_01_Python_Basics/YourFirstProgram.py
+++ Automate-The-Boring-Stuff-With-Python/Section_01_Python_Basics/YourFirstProgram.py
@@ -1,20 +1,19 @@
 """This program says hello and asks for the users name and age"""
 
-# display initial greeting
+# Display initial greeting
 print('Hello World!')
 
-# ask the user for their name
+# Ask the user for their Name and save it as a userName variable
 print('What is your name?')
-
-# store the user's input as their Name value
 userName = input()
 
-# greet user by their name and print the length of their name
+# Greet user by their name and print the length of their name
 print('It is good to meet you, ' + userName)
 print('Your name is ' + str(len(userName)) + ' characters long!')
 
-print('What is your age?')          # ask for their age
-# store the user's input as the Age value
+# Ask the user for their Age and save it as a userAge variable
+print('What is your age?')
 userAge = input()
+
 # print User's Age 1 year from now
 print('You will be ' + str(int(userAge) + 1) + ' in a year.')
