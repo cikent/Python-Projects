6d7e7c2a418e44598fa32cdb90206323901bb7e7
cikent <christopherikent@gmail.com>
2021-01-03 19:38:33 -0800

    Lesson 08 Continued
    Signed-off-by: cikent <christopherikent@gmail.com>

---
+++ Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_08_Built-In_Functions/Print_Random_example.py
@@ -0,0 +1,12 @@
+# Import a single module
+# import random
+
+# Import multiple modules at once 
+import random, sys, os, math        # Function calls from modules imported this way need to be prefaced by the module name
+
+# Import all the Functions associated with a given module
+from random import *                # Function calls from modules imported this way do NOT need to be prefared by the module
+
+# Create a for loop to print random #'s
+for i in range(5):
+    print(random.randint(1, 10))
--- Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_08_Built-In_Functions/printRandom.py
+++
@@ -1,9 +0,0 @@
-# Import Statement for a single module
-# import random
-
-# Import Statement for multiple modules at once
-import random, sys, os, math
-
-# Create a for loop to print random #'s
-for i in range(5):
-    print(random.randint(1, 10))
