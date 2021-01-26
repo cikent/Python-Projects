658cde8bb8da8e5620e683ded3cffd429f973ee8
cikent <christopherikent@gmail.com>
2021-01-26 07:55:14 -0800

    Lesson 11 Continued
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_11_Error_Handling/cats_example.py
+++ Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_11_Error_Handling/cats_example.py
@@ -1,7 +1,9 @@
 print('How many cats do you have?')
 numCats = input()
-
-if int(numCats) >=4:
-    print('That is a lot of cats.')
-else:
-    print('That is not that many cats.')
+try:
+    if int(numCats) >=4:
+        print('That is a lot of cats.')
+    else:
+        print('That is not that many cats.')
+except ValueError:
+    print('You did not enter a number.')
