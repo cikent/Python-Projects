ab583ece493a83159d80ca78ed9477f4bcc29798
Christopher Kent <christopherikent@gmail.com>
2021-01-29 07:49:37 -0800

    Working on Extra Credit
    Signed-off-by: Christopher Kent <christopherikent@gmail.com>

---
+++ Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_11_Error_Handling/Try_Except_ValueError_example-v2.py
@@ -0,0 +1,18 @@
+# Extra Credit: Create another Error to indicate when the User has inputted a negative number
+
+print('How many cats do you have?')
+numCats = input()
+
+try:
+    if type(numCats) is int:
+        try:
+            if int(numCats) >= 0:
+                if int(numCats) >= 4:
+                    print('That is a lot of cats.')
+                else:
+                    print('That is not that many cats.')
+        except:
+            print('You did not enter a positive number.')
+except ValueError:
+    print('You did not enter a number.')
+    
--- Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_11_Error_Handling/Try_Except_ValueError_example.py
+++ Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_11_Error_Handling/Try_Except_ValueError_example.py
@@ -4,8 +4,6 @@ numCats = input()
 try:
     if int(numCats) >= 4:
         print('That is a lot of cats.')
-    elif int(numCats) < 0:
-        print('Please input a positve number.')
     else:
         print('That is not that many cats.')
 except ValueError:
