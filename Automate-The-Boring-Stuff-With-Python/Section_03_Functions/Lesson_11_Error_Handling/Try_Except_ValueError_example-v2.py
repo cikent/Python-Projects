9d060fb05068f4305dd931cbeebf3802a9fa107e
cikent <christopherikent@gmail.com>
2021-02-03 07:57:21 -0800

    Extra Credit Continued
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_11_Error_Handling/Try_Except_ValueError_example-v2.py
+++ Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_11_Error_Handling/Try_Except_ValueError_example-v2.py
@@ -1,29 +1,26 @@
 # Extra Credit: Create another Error to indicate when the User has inputted a negative number
+def validateUserInteger(userVariable):
+    global numCats
 
-print('How many cats do you have?')
-numCats = validateUserInteger()
-
-try:
-    if type(numCats) is int:
+    try:
+        if numCats = int(numCats):
+            continue
+    except ValueError:
+        print('You did not enter a number.')
+        validateUserInteger()
         try:
             if int(numCats) >= 0:
-                if int(numCats) >= 4:
-                    print('That is a lot of cats.')
-                else:
-                    print('That is not that many cats.')
+                continue
         except:
-            print('You did not enter a positive number.')
-except ValueError:
-    print('You did not enter a number.')
-    
+                print('You did not enter a positive number.')
+                validateUserInteger()
 
-def validateUserInteger:
-    while True:
-        try:
-            if numCats = type(int):
-                True
-            else:
-                False
-        except:
-            print('Hello World')
 
+print('How many cats do you have?')
+numCats = input()
+validateUserInteger(numCats)
+
+if int(numCats) >= 4:
+    print('That is a lot of cats.')
+else:
+    print('That is not that many cats.')
