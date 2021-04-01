fa3c65a8631e2bf443db5ee3dd3f847056f483fb
Christopher Kent <christopherikent@gmail.com>
2021-04-01 07:55:12 -0700

    Completed Random Shuffle
    Signed-off-by: Christopher Kent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Section_06_Lists/Lesson_13_List_Data_Type/List_Random_Choice_Shuffle.py
+++ Automate-The-Boring-Stuff-With-Python/Section_06_Lists/Lesson_13_List_Data_Type/List_Random_Choice_Shuffle.py
@@ -2,4 +2,21 @@
 import random
 
 # Define a List variable
-myPets = ['Gunner', 'Bones', 'Dina', 'Fish', 'Nymeria']
+myPets = ['Bones', 'Dina', 'Fish', 'Gunner', 'Nymeria']
+
+# Print to console, myPets
+print('\nDebug: Print the List "myPets"')
+print(myPets)
+
+# Example 1: Randomly select an item from the List
+print('\nDebug: Print a random item from "myPets" using random.choice')
+print(random.choice(myPets))
+
+# Example 2: Randomly select an item from the List (this version is a longer version of Example 1)
+print('\nDebug: Print a random item from "myPets" using random.randint()')
+print(myPets[random.randint(0, len(myPets))-1])
+
+# Example 3: Randomly shuffle the items in the List
+print('\nDebug: Print the List "myPets", then print again after it has been shuffled using random.shuffle()')
+random.shuffle(myPets)
+print(myPets)
