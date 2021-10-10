d35acdb6fe4018715af47057a0af1ecf1068cea9
Christopher Kent <christopherikent@gmail.com>
2021-10-10 13:05:52 -0700

    Lesson 14 continued, Multiple Assignment, Variable Swaps
    Signed-off-by: Christopher Kent <christopherikent@gmail.com>

---
+++ Automate-The-Boring-Stuff-With-Python/Section_06_Lists/Lesson_14_ForLoops_wLists_MultipleAssignment_Augmented/MultipleAssignment_Variable_Swapping.py
@@ -0,0 +1,12 @@
+# Define Variables
+favoriteClass = 'Druid'
+favoriteArmor = 'Enigma'
+
+# Print the Variable values:
+print('This is the initial value of the two variables: ' + '\n1st Variables = ' + favoriteClass + '\n2nd Variables = ' + favoriteArmor)
+
+# Using Mutliple Assignment with Comma delimitation, you can swap variable values at the same time
+favoriteClass, favoriteArmor = favoriteArmor, favoriteClass
+
+# Print the Variable values:
+print('This is the swapped values of the two variables: ' + '\n1st Variables = ' + favoriteClass + '\n2nd Variables = ' + favoriteArmor)
