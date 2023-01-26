83b548eae7d458781bd23b35544c758430d9d084
Christopher Kent <christopherikent@gmail.com>
2023-01-26 07:07:20 -0800

    Section 7, Lession 18 continued, TicTacToe board page 120
    Signed-off-by: Christopher Kent <christopherikent@gmail.com>

---
+++ Automate-The-Boring-Stuff-With-Python/Section_07_Dictionaries/Lesson_18_Data_Structures/ticTacToe.py
@@ -0,0 +1,6 @@
+# Create a Tic Tac Toe game!
+
+# Define the Layout of the board as a Dictionary
+theBoard = {'top-L': '', 'top-M': '', 'top-R': '',
+ 'mid-L': '', 'mid-M': '', 'mid-R': '',
+  'low-L': '', 'low-M': '', 'low-R': ''}
