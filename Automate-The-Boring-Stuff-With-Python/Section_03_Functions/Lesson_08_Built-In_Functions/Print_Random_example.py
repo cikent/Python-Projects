437d006d4d004ae6a0bc8fdd86df1bd1ff7607a7
cikent <christopherikent@gmail.com>
2025-05-13 17:03:01 -0700

    Section 3, Lesson 8 updates
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_08_Built-In_Functions/Print_Random_example.py
+++ Automate-The-Boring-Stuff-With-Python/Section_03_Functions/Lesson_08_Built-In_Functions/Print_Random_example.py
@@ -5,7 +5,7 @@
 import random, sys, os, math        # Function calls from modules imported this way need to be prefaced by the module name
 
 # Import all the Functions associated with a given module
-from random import *                # Function calls from modules imported this way do NOT need to be prefared by the module
+from random import *                # Function calls from modules imported this way do NOT need to be prefaced by the module
 
 # Create a for loop to print random #'s
 for i in range(5):
