c21e5c82a2ff800da24a4bb5cec57f7fca3a3211
cikent <christopherikent@gmail.com>
2025-05-15 00:22:29 -0700

    Section 2, Lesson 6 updates
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Section_02_Flow_Control/Lesson_06_While_Loops/while_loop_example.py
+++ Automate-The-Boring-Stuff-With-Python/Section_02_Flow_Control/Lesson_06_While_Loops/while_loop_example.py
@@ -1,4 +1,9 @@
+# Declare the variable spam and set it to the Integer value of 0
 spam = 0
+
+# Execute While Loop while spam is less than 5
 while spam < 5:
+    # Print text to the console
     print('Hello World!')
+    # Increment the spam value by 1
     spam = spam + 1
