ccc98967886df88c1c20ab8cef7b8f83774b7c34
cikent <christopherikent@gmail.com>
2025-07-02 07:43:05 -0700

    Section 10, Lesson 24 update
    Signed-off-by: cikent <christopherikent@gmail.com>

---
+++ Automate-The-Boring-Stuff-With-Python/Section_10_Regular_Expressions/Lesson_24_Regex_Groups_Pipe_Character/RegexGroup-v1-example.py
@@ -0,0 +1,19 @@
+# Groups are created in regex strings with parentheses.
+# The first set of parentheses is group 1, the second is 2, and so on.
+# Calling group() or group(0) returns the full matching string, group(1) returns group 1's matching string, and so on.
+# Use \( and \) to match literal parentheses in the regex string.
+# The | pipe can match one of many possible groups.
+
+# import the regex module
+import re
+
+# Create a regex String object based upon a phone number value we want to identify WITH groups
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+
+# Declare String Variable
+exampleStringNoParentheses = 'My phone number is 415-555-4242'
+
+# Utilize the regex expression search method to create a new Match Object variable IF a pattern match is found
+matchObject1 = phoneNumRegex.search(exampleStringNoParentheses)
+
+print('The mo.object() value is: ' + matchObject1.group())
