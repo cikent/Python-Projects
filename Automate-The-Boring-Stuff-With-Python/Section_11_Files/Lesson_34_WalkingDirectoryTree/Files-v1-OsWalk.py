b6b3be01b8c154e24aa9d75b99c4042806be9bd7
cikent <christopherikent@gmail.com>
2025-08-04 10:49:06 -0700

    Section 11, Lesson 34, Clean-up completed
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Section_11_Files/Lesson_34_WalkingDirectoryTree/Files-v1-OsWalk.py
+++ Automate-The-Boring-Stuff-With-Python/Section_11_Files/Lesson_34_WalkingDirectoryTree/Files-v1-OsWalk.py
@@ -1,6 +1,16 @@
 # Import relevant Modules
 import os, shutil
 
+# Create a variable for a Backup Folder File Path
+backupFolderPath = 'C:\\Development\\Python-Projects\\Automate-The-Boring-Stuff-With-Python\\Section_11_Files\\Lesson_34_WalkingDirectoryTree\\BackupFiles'
+
+# Check if the Folder exists continue, otherwise create it
+if os.path.isdir(backupFolderPath):
+    # shutil.copy(os.path.join(folderName, file), os.path.join('C:\\Development\\Python-Projects\\Automate-The-Boring-Stuff-With-Python\\Section_11_Files\\Lesson_34_WalkingDirectoryTree\\BackupFiles', file + '.backup'))
+    shutil.rmtree(backupFolderPath)
+else:
+    os.makedirs(backupFolderPath)
+
 # Utilize os.walk() for a specified directory to Print all the Files and Subfolders contained therein
 for folderName, subfolders, filenames in os.walk('C:\\Development\\Python-Projects\\Automate-The-Boring-Stuff-With-Python'):
     print('The folder is: \n' + folderName)
@@ -26,10 +36,12 @@ for folderName, subfolders, filenames in os.walk('C:\\Development\\Python-Projec
             # v3 Comment In to create a local backup in the Current Working Directory
             shutil.copy(os.path.join(folderName, file), os.path.join('C:\\Development\\Python-Projects\\Automate-The-Boring-Stuff-With-Python\\Section_11_Files\\Lesson_34_WalkingDirectoryTree\\BackupFiles', file + '.backup'))
             
-
-    # Iterate through Subfolders and Delete the Backup Python Files
+    # NOT Used, for v1 Loop: Iterate through Subfolders and Delete the Backup Python Files
     for file in filenames:
         if file.endswith('.backup'):
             # Delete a file utilizing os.unlink() from the current working directory
             # os.unlink(file)                   # Debug Print the file to be deleted
             print('os.unlink(file)' + file)     # Debug Print the file to be deleted
+
+# Clean up Backup files created
+shutil.rmtree(backupFolderPath)         # Comment In to ensure the Copied files can be verified
