24554ba0c1e2149874cbb4365b2527cf8aaf0e77
cikent <christopherikent@gmail.com>
2025-08-15 04:03:09 -0700

    Section 13, Lesson 40 updates
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Section_13_Web_Scraping/Lesson_40_ParsingHTMLBeautifulSoupModule/ParsingHTML-v1-BeautifulSoupModule.py
+++ Automate-The-Boring-Stuff-With-Python/Section_13_Web_Scraping/Lesson_40_ParsingHTMLBeautifulSoupModule/ParsingHTML-v1-BeautifulSoupModule.py
@@ -7,7 +7,26 @@
 # The select() method will return a list of matching Element objects.
 
 # Import relelvant modules
-import bs4
+import bs4, requests
 
-# AmazonURL = 'https://www.amazon.com/Automate-Boring-Stuff-Python-Programming/dp/1593275994'
+# Add User Agent Header
+headers = {'User-Agent': 'Mozilla/5.0'}
 
+# Create a function() to obtain the Price for the Automate the Boring Stuff with Python book
+def getAmazonPrice(productUrl):
+    res = requests.get(productUrl)
+    res.raise_for_status()
+
+    # Create a Soup Object AND pass the html.parser as the 2nd argument to hide the warning
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    # Create an Element Selector for Price
+    elems = soup.select('#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center.aok-relative > span.aok-offscreen')
+    
+    # Return the Price Element Text value
+    return elems[0].text.strip()
+    
+# Create a variable for the Price
+price = getAmazonPrice('https://www.amazon.com/Automate-Boring-Stuff-Python-Programming/dp/1593275994')
+
+# Print to screen the Price value
+print('The price is ' + price)
