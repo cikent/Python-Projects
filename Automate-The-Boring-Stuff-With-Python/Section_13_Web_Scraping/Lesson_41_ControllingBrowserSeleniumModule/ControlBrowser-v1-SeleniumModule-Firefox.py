734d8f2dfc12f325ad8775af665a08a90932838a
cikent <christopherikent@gmail.com>
2025-08-21 15:02:17 -0700

    Section 13, Lesson 41 continued
    Signed-off-by: cikent <christopherikent@gmail.com>

--- Automate-The-Boring-Stuff-With-Python/Section_13_Web_Scraping/Lesson_41_ControllingBrowserSeleniumModule/ControlBrowser-v1-SeleniumModule-Firefox.py
+++ Automate-The-Boring-Stuff-With-Python/Section_13_Web_Scraping/Lesson_41_ControllingBrowserSeleniumModule/ControlBrowser-v1-SeleniumModule-Firefox.py
@@ -8,14 +8,64 @@
 # The submit() method will simulate clicking on the Submit button for a form.
 # The browser can also be controlled with these methods: back(), forward(), refresh(), quit().
 
-# Import relevant modules
+# Import relevant moduless
+import random
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-# Create a Firefox Browser Object
-browserFirefox = webdriver.Firefox()
+# Define Variables
+testUrl = 'https://automatetheboringstuff.com'
 
 # Create a Chrome Browser Object
-# browserChrome = webdriver.Chrome()
+# Specify the user agent string for emulation
+user_agent = [
+  "Mozilla/5.0 (Linux; Android 4.4.4; [HM NOTE|NOTE-III|NOTE2 1LTET) AppleWebKit/537.39 (KHTML, like Gecko)  Chrome/53.0.2111.335 Mobile Safari/536.6"
+  "Mozilla / 5.0 (compatible; MSIE 8.0; Windows; U; Windows NT 6.2; x64; en-US Trident / 4.0)"
+  "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_8; like Mac OS X) AppleWebKit/536.13 (KHTML, like Gecko)  Chrome/50.0.2440.333 Mobile Safari/600.1"
+  "Mozilla/5.0 (iPod; CPU iPod OS 11_1_7; like Mac OS X) AppleWebKit/603.49 (KHTML, like Gecko)  Chrome/51.0.1709.273 Mobile Safari/533.6"
+]
+s = Service(ChromeDriverManager().install())
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+chrome_options.add_argument('--user-agent=%s' % random.choice(user_agent))
+
+# Open Browser
+browser = webdriver.Chrome(service=s, options=chrome_options)
 
 # Navigate to a Site:
-browserFirefox.get('https://automatetheboringstuff.com')
+browser.get(testUrl)
+
+# Scroll to Element & Find the Table of Contents Introduction Hyperlink Element
+ActionChains(browser).move_to_element(WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.LINK_TEXT, "Introduction")))).perform()
+elemIntroduction = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.LINK_TEXT, "Introduction")))
+
+# Click the Introduction Hyperlink Element
+elemIntroduction.click()
+
+# Navigate back to the Home page
+browser.get(testUrl)
+
+# Find Home page Navbar Hamburger menu
+elemHamMenu = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "body > nav > div > button > span")))
+
+# Click the elemHamMenu Hyperlink Element
+elemHamMenu.click()
+
+# Find the Search Field & Send Keys
+elemSearchField = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#navbarSupportedContent > form > input.form-control.me-2")))
+elemSearchField.send_keys('zophie')
+
+# Submit the search term
+elemSearchField.submit()
+
+# Navigate the browser further
+browser.back()
+browser.forward()
+browser.refresh()
+browser.implicitly_wait(30)
+browser.quit()
